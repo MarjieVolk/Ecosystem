2aa64103ee1968b0efba8c7529d9bd4d261cf517
Taylor Bergquist <knexer@gmail.com>
2014-01-04 13:36:39 -0800

    Moved Allele to a different folder.

---
+++ EcosystemPrototype/Assets/Alleles/Allele.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Allele : MonoBehaviour {
+
+	public bool IsActive { get { return _isActive; } set
+		{
+			_isActive = value;
+			SetActive(value);
+		}
+	}
+	private bool _isActive;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	protected abstract void SetActive(bool active);
+}
