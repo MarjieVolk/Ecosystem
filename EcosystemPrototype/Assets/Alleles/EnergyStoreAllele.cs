ad4db49d47b0fc3b33d4361e9a3d62f71efc9fb0
Taylor Bergquist <knexer@gmail.com>
2014-01-07 13:15:37 -0800

    Created energy and nutrient storage alleles.
    They allow creatures to store energy and nutrients for later use.

---
+++ EcosystemPrototype/Assets/Alleles/EnergyStoreAllele.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyStoreAllele : Allele {
+
+	public int StartingEnergy;
+	public int MaxEnergy;
+
+	public int Energy{ get; }
+	private int _energy;
+
+	/// <summary>
+	/// Adds the energy.
+	/// </summary>
+	/// <returns>The energy that could not be added.</returns>
+	/// <param name="toBeAdded">The energy to be added.</param>
+	public int addEnergy(int toBeAdded)
+	{
+		int nextEnergy = _energy + toBeAdded;
+		int overflowEnergy = 0;
+		if (nextEnergy > MaxEnergy) {
+			overflowEnergy = nextEnergy - MaxEnergy;
+			nextEnergy = MaxEnergy;
+		}
+
+		return overflowEnergy;
+	}
+
+	/// <summary>
+	/// Removes the energy.
+	/// </summary>
+	/// <returns><c>true</c>, if energy was removed, <c>false</c> otherwise.</returns>
+	/// <param name="toBeRemoved">The energy to be removed.</param>
+	public bool removeEnergy(int toBeRemoved)
+	{
+		int nextEnergy = _energy - toBeRemoved;
+		if (nextEnergy < 0) {
+			return false;
+		}
+
+		_energy = nextEnergy;
+		return true;
+	}
+
+	// Use this for initialization
+	void Start () {
+		_energy = StartingEnergy;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ EcosystemPrototype/Assets/Alleles/NutrientStoreAllele.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NutrientStoreAllele : Allele {
+
+    public int Capacity;
+
+    private Dictionary<Nutrient, int> nutrientStore;
+
+    void Start()
+    {
+        nutrientStore = new Dictionary<Nutrient, int>();
+    }
+
+    /// <summary>
+    /// Adds nutrients to the NutrientStore.
+    /// </summary>
+    /// <param name="nutrient">
+    /// The type of nutrient to be added.
+    /// </param>
+    /// <param name="amount">
+    /// The amount of the nutrient to be added.
+    /// </param>
+    /// <returns>
+    /// The amount of the nutrient that could NOT be added, e.g. 0 if all of the nutrients could be added.
+    /// </returns>
+    public int AddNutrients(Nutrient nutrient, int amount)
+    {
+        if (nutrientStore[nutrient] == null)
+        {
+            nutrientStore[nutrient] = 0;
+        }
+
+        int nextAmount = amount + nutrientStore[nutrient];
+        int overflow = 0;
+        if (nextAmount > Capacity)
+        {
+            overflow = nextAmount - Capacity;
+            nextAmount = Capacity;
+        }
+
+        nutrientStore[nutrient] = nextAmount;
+
+        return overflow;
+    }
+
+    public void GetNutrients(Nutrient nutrient)
+    {
+        return nutrientStore[nutrient];
+    }
+
+    /// <summary>
+    /// Removes amount of nutrient from the nutrient store, or nothing at all if that's impossible.
+    /// </summary>
+    /// <param name="nutrient">
+    /// The type of nutrient to be removed.
+    /// </param>
+    /// <param name="amount">
+    /// The amount of the nutrient desired.
+    /// </param>
+    /// <returns>
+    /// true if all nutrients requested were removed, false otherwise.
+    /// </returns>
+    public bool RemoveNutrients(Nutrient nutrient, int amount)
+    {
+        if(nutrientStore[nutrient] == null || nutrientStore[nutrient] < amount)
+        {
+            return false;
+        }
+
+        nutrientStore[nutrient] -= amount;
+        return true;
+}
