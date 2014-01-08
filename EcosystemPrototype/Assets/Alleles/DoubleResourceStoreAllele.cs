fe53ff0364219f40e09eea400f6c24a35d55203c
Taylor Bergquist <knexer@gmail.com>
2014-01-08 10:19:24 -0800

    Created alleles for wrapping the resource stores

---
+++ EcosystemPrototype/Assets/Alleles/DoubleResourceStoreAllele.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Alleles
+{
+    public class DoubleResourceStoreAllele<T> : Allele
+    {
+        public DoubleResourceStore<T> Store { get; private set; }
+
+        public T Type;
+        public double Capacity, InitialAmount;
+
+        void Start()
+        {
+            Store = new DoubleResourceStore<T>(Type, Capacity, InitialAmount);
+        }
+    }
+}
---
+++ EcosystemPrototype/Assets/Alleles/IntegerResourceStoreAllele.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Alleles
+{
+    public class IntegerResourceStoreAllele<T> : Allele
+    {
+        public IntegerResourceStore<T> Store { get; private set; }
+
+        public readonly T Type;
+        public readonly int Capacity, InitialAmount;
+
+        void Start()
+        {
+            Store = new IntegerResourceStore<T>(Type, Capacity, InitialAmount);
+        }
+    }
+}
