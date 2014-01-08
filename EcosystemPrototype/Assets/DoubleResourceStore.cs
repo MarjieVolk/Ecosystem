f5815c9e4a5505444838673c518f8b43235c1468
Taylor Bergquist <knexer@gmail.com>
2014-01-08 09:23:11 -0800

    Implemented generic resource storage
    Refactor process begin!

---
+++ EcosystemPrototype/Assets/DoubleResourceStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    public class DoubleResourceStore<T>
+    {
+        public T StoredResource { get; private set; }
+        public double Capacity { get; private set; }
+        public double Amount { get; private set; }
+
+        public DoubleResourceStore(T type, int capacity, int initalAmount)
+        {
+            StoredResource = type;
+            Capacity = capacity;
+            Amount = initalAmount;
+        }
+
+        public bool addResource(double amount)
+        {
+            double nextAmount = Amount + amount;
+            if (nextAmount > Capacity) return false;
+            Amount = nextAmount;
+            return true;
+        }
+
+        public bool removeResource(double amount)
+        {
+            double nextAmount = Amount - amount;
+            if (nextAmount < 0) return false;
+            Amount = nextAmount;
+            return true;
+        }
+    }
+}
---
+++ EcosystemPrototype/Assets/IntegerResourceStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    public class IntegerResourceStore<T, V>
+    {
+        public T StoredResource { get; private set; }
+        public int Capacity { get; private set; }
+        public int Amount { get; private set; }
+
+        public IntegerResourceStore(T type, int capacity, int initalAmount)
+        {
+            StoredResource = type;
+            Capacity = capacity;
+            Amount = initalAmount;
+        }
+
+        public bool addResource(int amount)
+        {
+            int nextAmount = Amount + amount;
+            if (nextAmount > Capacity) return false;
+            Amount = nextAmount;
+            return true;
+        }
+
+        public bool removeResource(int amount)
+        {
+            int nextAmount = Amount - amount;
+            if (nextAmount < 0) return false;
+            Amount = nextAmount;
+            return true;
+        }
+    }
+}
