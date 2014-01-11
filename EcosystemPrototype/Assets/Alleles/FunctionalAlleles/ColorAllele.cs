20a11ecf813aa930c05126cd21f03de66979dfac
Taylor Bergquist <knexer@gmail.com>
2014-01-10 20:28:25 -0800

    Created a color allele to inherit material
    Also added it to the plant we've been using

---
+++ EcosystemPrototype/Assets/Alleles/FunctionalAlleles/ColorAllele.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Alleles.FunctionalAlleles
+{
+    public class ColorAllele : Allele
+    {
+        [GeneticallyInheritable]
+        public Material Color;
+
+        void Start()
+        {
+            gameObject.renderer.material = Color;
+        }
+    }
+}
