7f326fa9ca75423e8823aa3a982830ee2b297af9
Taylor Bergquist <knexer@gmail.com>
2014-01-11 17:56:08 -0800

    Implemented plant death
    They now slowly consume energy over time, die when they're out of
    energy, and deposit the nutrients they have store on their tile when
    they die.

---
+++ EcosystemPrototype/Assets/Alleles/FunctionalAlleles/BaseMetabolismAllele.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Alleles.FunctionalAlleles
+{
+    public class BaseMetabolismAllele : Allele
+    {
+        [GeneticallyInheritable]
+        public double MetabolicRate;
+
+        private DoubleResourceStore energy; 
+
+        private const string ENERGY_STORE = "energystore";
+
+        public void Start()
+        {
+            Genome genome = this.gameObject.GetComponent<Genome>();
+            energy = ((DoubleResourceStoreAllele)genome.GetActiveAllele(ENERGY_STORE)).Store;
+        }
+
+        public void Update()
+        {
+            if (!energy.removeResource(MetabolicRate))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ EcosystemPrototype/Assets/Alleles/FunctionalAlleles/DepositNutrientsOnTileOnDeathAllele.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Alleles.FunctionalAlleles
+{
+    public class DepositNutrientsOnTileOnDeathAllele : Allele
+    {
+        [GeneticallyInheritable]
+        public string NutrientStoreGeneBase;
+
+        public void OnStart()
+        {
+        }
+
+        public void OnDestroy()
+        {
+            Console.WriteLine("dead executed");
+            Tile closestTile = TileManager.instance.getTileClosestTo(transform.position);
+            foreach (Nutrient nutrient in Enum.GetValues(typeof(Nutrient)))
+            {
+                IntegerResourceStoreAllele storeAllele = ((IntegerResourceStoreAllele)genome.GetActiveAllele(NutrientStoreGeneBase + Genome.GENE_DELIMITER + nutrient));
+                if(storeAllele == null ) continue;
+                IntegerResourceStore store = storeAllele.Store;
+                int amount = store.Amount;
+                store.removeResource(amount);
+                closestTile.addNutrient(nutrient, amount);
+            }
+        }
+    }
+}
